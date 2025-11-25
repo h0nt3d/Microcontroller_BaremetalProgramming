069a7ce692582a06e08fef1127dc91f4042e06f3
h0nt3d <akanoim@gmail.com>
2025-11-25 11:35:40 -0400

    Added ports.h for ESP32_PSUEDO-RANDOM-NUMBER-GENERATOR

--- ESP32_PSUEDO-RANDOM-NUMBER-GENERATOR/main/ESP32_PSUEDO-RANDOM-NUMBER-GENERATOR.c
+++ ESP32_PSUEDO-RANDOM-NUMBER-GENERATOR/main/ESP32_PSUEDO-RANDOM-NUMBER-GENERATOR.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include <time.h>
+#include "ports.h"
 
-#define GPIO_OUT_W1TS_REG 	0x3FF44008
-#define GPIO_OUT_W1TC_REG 	0x3FF4400C
-#define GPIO_ENABLE_REG 	0x3FF44020
-#define GPIO_PIN_2		2
-#define GPIO_PIN_4		4
-#define GPIO_PIN_5              5
-#define GPIO_PIN_16		16
-#define GPIO_PIN_17             17
-#define GPIO_PIN_18             18
-#define GPIO_PIN_19             19
-#define DELAY_MS		1000
-
-/*
- * Pins:
- * 2 - a | 4 - b | 5 - c | 16 - d | 17 - e | 18 - f | 19 - g
-*/ 
+#define DELAY_MS 	1000
 
 void app_main(void)
 {
