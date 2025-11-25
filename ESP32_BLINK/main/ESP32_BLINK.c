8489a18cba641ef763e0d6318c42359e8c8c9ba9
h0nt3d <akanoim@gmail.com>
2025-11-25 09:25:29 -0400

    Added ports header file in ESP32_BLINK

--- ESP32_BLINK/main/ESP32_BLINK.c
+++ ESP32_BLINK/main/ESP32_BLINK.c
@@ -1,18 +1,8 @@
-/**
- * ESP32-U
- * List of Working IO Pins:
- * 2, 4, 5, 16, 17, 18, 19, 21, 22, 23, 27, 26, 25 
- */
-
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
+#include "ports.h"
 
-
-#define GPIO_OUT_W1TS_REG 	0x3FF44008
-#define GPIO_OUT_W1TC_REG 	0x3FF4400C
-#define GPIO_ENABLE_REG 	0x3FF44020
-#define GPIO5			5
-#define DELAY_MS		1000
+#define DELAY_MS	1000
 
 void app_main(void)
 {
